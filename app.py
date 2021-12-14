ecebcca4b0cd837640eed32641f30a38002d926a
inbox.amitraj@gmail.com <inbox.amitraj@gmail.com>
2021-12-14 17:55:27 +0800

    sg-time

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import requests
 from flask import Flask, render_template
 
-# r = requests.get('https://api.github.com/user', auth=('<YourUserName>', '<YourToken>'))
-r = requests.get('https://reqres.in/api/users?page=1')
 
-data = r.json()
-print(data)
 app = Flask(__name__)
 
 
+@app.route('/users')
+def user_list():
+    # r = requests.get('https://api.github.com/user', auth=('<YourUserName>', '<YourToken>'))
+    user_list_request = requests.get('https://reqres.in/api/users?page=1')
+    user_list_data = user_list_request.json()
+    print(user_list_data)
+    return render_template("app.html", rows=user_list_data)
+
+
 @app.route('/')
-def hello():
-    return render_template("app.html", rows=data)
+def sg_time():
+    sg_time_request = requests.get('http://worldtimeapi.org/api/timezone/Asia/Singapore')
+    sg_time_data = sg_time_request.json()
+    print(sg_time_data)
+    return render_template("sg_time.html", rows=sg_time_data)
 
 
 if __name__ == '__main__':
